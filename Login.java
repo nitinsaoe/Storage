a301a71f0b585b28840f9021e6c4ad45b49ed1dd
nitinsaoe <79371524+nitinsaoe@users.noreply.github.com>
2021-02-20 22:10:03 +0530

    Login
    login selenium code

---
+++ Login.java
@@ -0,0 +1,37 @@
+package com.sam.java;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Login {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriverManager.chromedriver().setup();
+		
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("http://leaftaps.com/opentaps/control/main");
+		driver.manage().window().maximize();
+		
+		String title = driver.getTitle();
+		System.out.println(title);
+		
+		driver.findElement(By.id("username")).sendKeys("demosalesmanager");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		driver.findElement(By.linkText("Create Lead")).click();
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys("Accenture Solution pvt ltd");
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Nitin");
+		driver.findElement(By.id("createLeadForm_lastName")).sendKeys("Saurav");
+		driver.findElement(By.name("submitButton")).click();
+		String title1 = driver.getTitle();
+		
+		
+
+		System.out.println(title1);
+	}
+
+}
